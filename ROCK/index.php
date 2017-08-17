5f1e745139dbbebc4a52690ab3eaaf4324ce787c
Amina Belabbes <30630103+Programina@users.noreply.github.com>
2017-08-17 13:21:03 +0200

    Basic Animated Site
    Work in progress

---
+++ ROCK/index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Coding Rocks
+		</title>
+		<link rel="stylesheet" type="text/css" href="comicstyle.css"/>
+	</head>
+
+	<body>
+		<div class="container">
+			<img src="Rock1.png" height="50%" width="30%" />
+		</div>
+<!--button-->
+		<div class="container">
+			<div class="button1">
+				<a href="tagesschau.de">
+					Take the Tour
+				</a>
+			</div>
+		</div>
+	</body>
+</html>
